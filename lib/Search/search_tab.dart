1abd94dce423ce6625a15937141cbfcb0f506483
DELL <alaaaymanfouad@gmail.com>
2023-10-27 08:22:02 +0200

    search purple background

--- lib/Search/search_tab.dart
+++ lib/Search/search_tab.dart
@@ -6,7 +6,7 @@ class SearchTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color:Colors.red,
+      color:Colors.purple,
     ) ;
   }
 }
