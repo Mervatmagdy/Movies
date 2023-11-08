89c49e4d26ef75e0d92ee3d12ed5be67c501b9b7
DELL <alaaaymanfouad@gmail.com>
2023-11-08 09:47:28 +0200

    Browser tab finished

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:movies/Home/home_screen.dart';
 import 'package:movies/MyTheme.dart';
 import 'package:movies/Search/search_tab.dart';
 import 'package:movies/WatchList/watch_tab.dart';
+import 'Browser/movie/movie_details.dart';
 
 void main(){
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +20,10 @@ class MyApp extends StatelessWidget{
         HomeScreen.routeName:(context) => HomeScreen(),
         SearchTab.routeName:(context) => SearchTab(),
         BrowserTab.routeName:(context) => BrowserTab(),
-        WatchTab.routeName:(context) => WatchTab()
+        WatchTab.routeName:(context) => WatchTab(),
+        MovieDetails.routeName:(context) => MovieDetails(),
+       
+
       } ,
       theme:MyTheme.lightTheme,
     );
---
+++ lib/model/category_image.dart
@@ -0,0 +1,20 @@
+import 'MovieGenreResponse.dart';
+
+class CategoryImage{
+  String ImageUrl;
+  CategoryImage({ required this.ImageUrl});
+
+static List<CategoryImage> getGenres2(List<Genres> genreList) {
+  List<CategoryImage> newGenreList = [];
+  CategoryImage categoryImage;
+  for(int i = 0 ; i<genreList.length ; i++){
+    categoryImage =CategoryImage( ImageUrl: 'assets/images/${genreList[i].name}.jpg');
+    newGenreList.add(categoryImage);
+  }
+  return newGenreList;
+}
+
+  }
+  
+
+
